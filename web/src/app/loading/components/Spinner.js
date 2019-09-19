ba88fc5266a28fca2a99c4a8dee751a6b48f231d
Nathaniel Caza <mastercactapus@gmail.com>
2019-09-19 16:31:12 -0500

    add `useSpin` hook

--- web/src/app/loading/components/Spinner.js
+++ web/src/app/loading/components/Spinner.js
@@ -1,58 +1,93 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import p from 'prop-types'
 import CircularProgress from '@material-ui/core/CircularProgress'
 
 import { DEFAULT_SPIN_DELAY_MS, DEFAULT_SPIN_WAIT_MS } from '../../config'
 
-/*
- * Show a loading spinner in the center of the container.
- */
-export default class Spinner extends React.PureComponent {
-  static propTypes = {
-    // Wait `delayMs` milliseconds before rendering a spinner.
-    delayMs: p.number,
+export function useSpin(
+  loading,
+  { delayMs = DEFAULT_SPIN_DELAY_MS, waitMs = DEFAULT_SPIN_WAIT_MS },
+) {
+  const [lastStartSpin, setLastStartSpin] = useState(Date.now())
+  const [lastEndSpin, setLastEndSpin] = useState(Date.now())
 
-    // Wait `waitMs` before calling onReady.
-    waitMs: p.number,
+  const [renderSpin, setRenderSpin] = useState(!delayMs && loading) // start spinning if delayMs is 0
 
-    // onSpin is called when the spinner starts spinning.
-    onSpin: p.func,
-
-    // onReady is called once the spinner has spun for `waitMs`.
-    onReady: p.func,
+  const startSpinning = () => {
+    if (renderSpin) return
+    setRenderSpin(true)
+    setLastStartSpin(Date.now())
+  }
+  const stopSpinning = () => {
+    if (!renderSpin) return
+    setRenderSpin(false)
+    setLastEndSpin(Date.now())
   }
 
-  static defaultProps = {
-    delayMs: DEFAULT_SPIN_DELAY_MS,
-    waitMs: DEFAULT_SPIN_WAIT_MS,
-  }
+  useEffect(() => {
+    if (loading) {
+      const wait = lastEndSpin + delayMs - Date.now()
+      if (wait < 1) return startSpinning()
+      const t = setTimeout(startSpinning, wait)
+      return () => clearTimeout(t)
+    }
 
-  state = {
-    spin: false,
-  }
+    const wait = lastStartSpin + waitMs - Date.now()
+    if (wait < 1) return stopSpinning()
+    const t = setTimeout(stopSpinning, wait)
+    return () => clearTimeout(t)
+  }, [loading])
 
-  componentDidMount() {
-    this._spin = setTimeout(() => {
-      this._spin = null
-      this.setState({ spin: true })
-      if (this.props.onSpin) this.props.onSpin()
+  return renderSpin
+}
 
-      if (this.props.waitMs && this.props.onReady) {
-        this._spin = setTimeout(this.props.onReady, this.props.waitMs)
-      }
-    }, this.props.delayMs)
-  }
-  componentWillUnmount() {
-    clearTimeout(this._spin)
-  }
+export function ContainerSpinner() {
+  return (
+    <div style={{ position: 'absolute', top: '50%', left: '50%' }}>
+      <CircularProgress />
+    </div>
+  )
+}
+export function TextSpinner({ value }) {
+  return (
+    <span style={{ height: '1em', color: 'gray' }}>
+      <CircularProgress size='1em' />
+      &nbsp;{value}
+    </span>
+  )
+}
 
-  render() {
-    if (this.props.delayMs && !this.state.spin) return null
+export default function Spinner({
+  delayMs = DEFAULT_SPIN_DELAY_MS,
+  waitMs = DEFAULT_SPIN_WAIT_MS,
+  onSpin = () => {},
+  onReady = () => {},
+}) {
+  const spin = useSpin(true, { delayMs, waitMs })
+  useEffect(() => {
+    const spT = setTimeout(onSpin, delayMs)
+    const orT = setTimeout(onReady, delayMs + waitMs)
 
-    return (
-      <div style={{ position: 'absolute', top: '50%', left: '50%' }}>
-        <CircularProgress />
-      </div>
-    )
-  }
+    return () => {
+      clearTimeout(spT)
+      clearTimeout(orT)
+    }
+  }, [])
+
+  if (!spin) return null
+
+  return <ContainerSpinner />
+}
+Spinner.propTypes = {
+  // Wait `delayMs` milliseconds before rendering a spinner.
+  delayMs: p.number,
+
+  // Wait `waitMs` before calling onReady.
+  waitMs: p.number,
+
+  // onSpin is called when the spinner starts spinning.
+  onSpin: p.func,
+
+  // onReady is called once the spinner has spun for `waitMs`.
+  onReady: p.func,
 }
